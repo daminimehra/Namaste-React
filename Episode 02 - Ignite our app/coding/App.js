496f7e6038cb2148f8488ecaa107698919949510
Damini Mehra <71871235+daminimehra@users.noreply.github.com>
2023-10-06 21:29:54 +0530

    Add files via upload

---
+++ Episode 02 - Ignite our app/coding/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+const parent = React.createElement(
+    "div" ,
+     {id:"parent"}, 
+React.createElement("div" ,{id:"child"},
+[  React.createElement("h1", {}, "h1 tag"), 
+React.createElement("h2", {}, "h2 tag")
+])
+  );
+
+  console.log(parent); //ReactElement(object)=> HTML
+
+const heading = React.createElement("h1", {id:"heading"}, "hello with react");
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(parent);
